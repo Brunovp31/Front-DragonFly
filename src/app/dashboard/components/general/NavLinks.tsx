b3e962be401217a3c98aa1385358508183eba322
Diego Cedron <70548640+DiegoCedron7@users.noreply.github.com>
2024-11-08 01:01:45 -0500

    Add tooltips and animations to navigation links; update link names to Spanish

--- src/app/dashboard/components/general/NavLinks.tsx
+++ src/app/dashboard/components/general/NavLinks.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useAuth } from "@/app/context/auth-context";
+import { Tooltip } from "@nextui-org/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { GrUserWorker } from "react-icons/gr";
@@ -8,6 +9,7 @@ import { IoHomeOutline } from "react-icons/io5";
 import { MdDeliveryDining, MdProductionQuantityLimits } from "react-icons/md";
 import { RiOrderPlayLine } from "react-icons/ri";
 import { RxActivityLog, RxDashboard, RxPerson } from "react-icons/rx";
+import { motion } from "framer-motion";
 
 const permitAll = ["ADMIN", "WORKER", "DELIVERY", "GARDENER"];
 const links = [
@@ -19,43 +21,43 @@ const links = [
     permit: permitAll,
   },
   {
-    name: "Products",
+    name: "Productos",
     href: "/dashboard/products",
     icon: RxDashboard,
     permit: ["ADMIN", "WORKER"],
   },
   {
-    name: "Categories",
+    name: "Categorias",
     href: "/dashboard/categories",
     icon: RxActivityLog,
     permit: ["ADMIN", "WORKER"],
   },
   {
-    name: "Users",
+    name: "Usuarios",
     href: "/dashboard/users",
     icon: RxPerson,
     permit: ["ADMIN"],
   },
   {
-    name: "Workers",
+    name: "Trabajadores",
     href: "/dashboard/workers",
     icon: GrUserWorker,
     permit: ["ADMIN"],
   },
   {
-    name: "Productions",
+    name: "Producciones",
     href: "/dashboard/productions",
     icon: MdProductionQuantityLimits,
     permit: ["GARDENER", "ADMIN"],
   },
   {
-    name: "Deliveries",
+    name: "Entregas",
     href: "/dashboard/deliveries",
     icon: MdDeliveryDining,
     permit: ["DELIVERY", "ADMIN"],
   },
   {
-    name: "Orders",
+    name: "Pedidos",
     href: "/dashboard/orders",
     icon: RiOrderPlayLine,
     permit: ["ADMIN", "WORKER"],
@@ -70,26 +72,37 @@ export default function NavLinks() {
   return (
     <>
       {links
-        .filter((link) => link.permit.some((role) => roles.includes(role))) // Filtra enlaces según los permisos del usuario
+        .filter((link) => link.permit.some((role) => roles.includes(role)))
         .map((link) => {
           const LinkIcon = link.icon;
           const isHome = link.name === "Home";
           const isSelected = pathname === link.href;
 
           return (
-            <Link key={link.name} href={link.href} passHref>
-              <div
-                className={`flex h-[48px] items-center justify-center gap-2 rounded-lg bg-gray-100 cursor-pointer my-4 p-3 md:flex-none md:justify-start md:p-2 md:px-3 transition-colors duration-500 ${
-                  isHome ? link.bg : ""
-                } ${isHome ? "text-white" : ""} ${
-                  isSelected && !isHome ? "bg-gray-300" : ""
-                } ${!isSelected ? "hover:bg-gray-400" : ""} ${
-                  isHome && !isSelected ? "hover:bg-purple-700" : ""
-                }`}
-              >
-                <LinkIcon size={20} />
-              </div>
-            </Link>
+            <Tooltip
+              key={link.name}
+              content={link.name}
+              placement="right"
+              color="foreground"
+            >
+              <Link href={link.href} passHref>
+                <motion.div
+                  initial={{ opacity: 0, y: 10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  whileHover={{ scale: 1.1 }}
+                  transition={{ type: "spring", stiffness: 300 }}
+                  className={`flex h-[48px] items-center justify-center gap-2 rounded-lg bg-gray-100 cursor-pointer my-4 p-3 md:flex-none md:justify-start md:p-2 md:px-3 transition-colors duration-500 ${
+                    isHome ? link.bg : ""
+                  } ${isHome ? "text-white" : ""} ${
+                    isSelected && !isHome ? "bg-gray-300" : ""
+                  } ${!isSelected ? "hover:bg-gray-400" : ""} ${
+                    isHome && !isSelected ? "hover:bg-purple-700" : ""
+                  }`}
+                >
+                  <LinkIcon size={20} />
+                </motion.div>
+              </Link>
+            </Tooltip>
           );
         })}
     </>
